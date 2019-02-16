09cd5aeab696ed7c15a38e3c61b7edc9c9a9ee36
Antoine Lefevre <antoine.lefevre@supinternet.fr>
2019-02-16 11:35:30 +0100

    init project

---
+++ Random User/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random User
+//
+//  Created by Antoine Lefevre on 16/02/2019.
+//  Copyright © 2019 Antoine Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Random UserTests/Random_UserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Random_UserTests.swift
+//  Random UserTests
+//
+//  Created by Antoine Lefevre on 16/02/2019.
+//  Copyright © 2019 Antoine Lefevre. All rights reserved.
+//
+
+import XCTest
+@testable import Random_User
+
+class Random_UserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
